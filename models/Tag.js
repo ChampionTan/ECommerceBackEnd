a7a2d94cde147d54c1639e7270c0209788881b76
unknown <tannerwlumsden@gmail.com>
2023-01-16 19:47:57 -0500

    added tag model

--- models/Tag.js
+++ models/Tag.js
@@ -4,8 +4,17 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init(
-  {
+Tag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+
+  }
     // define columns
   },
   {
